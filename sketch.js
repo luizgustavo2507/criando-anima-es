bcb5d046af12c3ca6ce6e7feac0df1f2cfd0fe7b
luizgustavo2507 <163295683+luizgustavo2507@users.noreply.github.com>
2024-03-15 08:36:37 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,41 @@
+let cor;
+let posicaoHorizontal; // x
+let posicaoVertical; // y
+
+function setup() {
+  createCanvas(550, 550);
+   background(color(300,0,0));
+  cor = color(random(0,255), random(0,255), random(0,255));
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+
+function draw() {
+ 
+  fill(cor);
+  circle(posicaoHorizontal,posicaoVertical,67);
+ 
+ 
+ 
+  if (mouseX < posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal - 1;
+  }
+ 
+  if (mouseX > posicaoHorizontal){
+    posicaoHorizontal =  posicaoHorizontal + 1;
+  }
+ 
+  if (mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+   
+  if (mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+ 
+  if (mouseIsPressed){
+    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
+  }
+   
+}
